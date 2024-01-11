860054696ccd6bc788f60c686d1511cdc68a8670
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2024-01-11 16:27:30 +0200

    Task 5

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -5,6 +5,15 @@ Module Docs
 
 
 def sum_list(input_list: list[float]) -> float:
+    """
+    Calculates the sum of a list of floats.
+
+    Parameters:
+    - input_list (List[float]): The list of floats.
+
+    Returns:
+    float: The sum of the input list.
+    """
     result: float = 0.0
     for i in input_list:
         result += i
