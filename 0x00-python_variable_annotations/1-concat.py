385a4a888c1c0ec1dff571debb2dc0709b1c6fbe
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2024-01-11 16:09:46 +0200

    Task 1: docs fix

--- 0x00-python_variable_annotations/1-concat.py
+++ 0x00-python_variable_annotations/1-concat.py
@@ -5,4 +5,14 @@ Module Docs
 
 
 def concat(str1: str, str2: str):
+    """
+    Concatenates two strings and returns the result.
+
+    Parameters:
+    - str1 (str): The first string.
+    - str2 (str): The second string.
+
+    Returns:
+    str: The concatenated string of str1 and str2.
+    """
     return str1 + str2
