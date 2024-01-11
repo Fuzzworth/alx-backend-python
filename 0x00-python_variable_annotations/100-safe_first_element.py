c4788eef7fb37aab71e9a178747b504127e4fdf6
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2024-01-11 17:33:57 +0200

    Task 10

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -2,7 +2,20 @@
 '''
 Module Doc
 '''
-from typing import List, Union
+from typing import Sequence, Union, Any
 
 
+def safe_first_element(lst: Sequence[Any]) ->Union[Any, NoneType]:
+    """
+    Returns the first element of the input sequence, or None if the sequence is empty.
 
+    Parameters:
+    - lst (Sequence[Any]): The input sequence.
+
+    Returns:
+    Union[Any, None]: The first element of the input sequence, or None if the sequence is empty.
+    """
+    if lst:
+        return lst[0]
+    else:
+        return None
