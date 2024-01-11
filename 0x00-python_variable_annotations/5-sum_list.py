9859439db414aa2428cd0bdeb42f80ead6119345
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2024-01-11 16:31:52 +0200

    Task 5: fix 4

--- 0x00-python_variable_annotations/5-sum_list.py
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -2,9 +2,10 @@
 '''
 Module Docs
 '''
+from typing import List
 
 
-def sum_list(input_list: list[float]) -> float:
+def sum_list(input_list: List[float]) -> float:
     """
     Calculates the sum of a list of floats.
 
