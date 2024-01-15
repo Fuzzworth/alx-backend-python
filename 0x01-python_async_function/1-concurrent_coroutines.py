3e9c894c0a428f1023e1007d3669ab57c80d1b54
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2024-01-15 23:40:43 +0200

    Task 1

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Module Docs
+"""
+import asyncio
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> float:
+    """
+    Function Docs
+    """
+    list_of_delays = []
+    for _ in range(n):
+        list_of_delays.append(await wait_random(max_delay))
+    return list_of_delays
