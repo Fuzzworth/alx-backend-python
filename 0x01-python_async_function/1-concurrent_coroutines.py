b4fe9e7517991ba248a9835f032ab721b152e8a3
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2024-01-15 23:50:10 +0200

    Task 1 Fix: 4

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -3,11 +3,12 @@
 Module Docs
 """
 import asyncio
+from typing import List
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> float:
+async def wait_n(n: int, max_delay: int) -> iList[float]:
     """
     Function Docs
     """
