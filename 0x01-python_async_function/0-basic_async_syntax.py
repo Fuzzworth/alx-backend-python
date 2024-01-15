6e7ce8eaa3acc37093a0b81c1b971b2eb4e0814e
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2024-01-15 23:33:59 +0200

    Task 0

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -11,5 +11,5 @@ async def wait_random(max_delay: int = 10) -> float:
     Function Docs
     """
     random_delay: float = uniform(0, max_delay)
-    asyncio.sleep(random_delay)
+    await asyncio.sleep(random_delay)
     return random_delay
