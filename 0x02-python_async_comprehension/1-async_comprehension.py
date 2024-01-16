c44e0e2d2b6527fbb0d6cec7d832b1be6eaed83f
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2024-01-16 20:54:03 +0200

    Task 1

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -3,11 +3,11 @@
 Module Docs
 """
 import asyncio
+from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-from typing import List
 
 async def async_comprehension() -> List[float]:
     """
