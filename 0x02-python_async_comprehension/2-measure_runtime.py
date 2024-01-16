7607645938425ab66bffe3acf4480976a123a285
Nyangasi Mhozya <nyangasi.m@hotmail.com>
2024-01-16 20:57:42 +0200

    Task 2

--- 0x02-python_async_comprehension/2-measure_runtime.py
+++ 0x02-python_async_comprehension/2-measure_runtime.py
@@ -7,6 +7,7 @@ from typing import List
 
 async_comprehension = __import__('1-async_comprehension').async_comprehension
 
+
 async def measure_runtime() -> float:
     """
     Coroutine that executes async_comprehension four times in parallel using
